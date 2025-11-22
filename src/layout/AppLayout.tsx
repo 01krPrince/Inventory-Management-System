fd3a2c2814790b35f5e819cefdb7303f4f94c47d
Prince Kumar <prince.infoera@gmail.com>
2025-11-22 18:13:05 +0530

    build fixed

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -14,7 +14,7 @@ import PartyBrandwiseDiscountCharges from "../pages/pages/sales/salesPriceList/P
 import UpdateListForEachItems from "../pages/pages/sales/salesPriceList/UpdateListForEachItems";
 import UpdateBarcodeRate from "../pages/pages/sales/salesPriceList/UpdateBarcodeRate";
 import SalesInvoice from "../pages/pages/sales/salesInvoice/SalseInvoice";
-import ItemMaster from "../pages/pages/inventory/itemMaster/itemMaster";
+import ItemMaster from "../pages/pages/inventory/itemMaster/ItemMaster";
 import { CustomCalculator } from "../components/CustomCalculator";
 
 const useSidebar = () => ({
